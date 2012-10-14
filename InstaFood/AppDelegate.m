4966bef2ed13f19aee6313c22a29f0e0622e0554
blurboy1985 <daniel.tianwen@gmail.com>
2012-10-14 13:45:53 +0800

    init commit

--- InstaFood/AppDelegate.m
+++ InstaFood/AppDelegate.m
@@ -13,6 +13,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    application.statusBarHidden = NO;
+    
     return YES;
 }
 							
